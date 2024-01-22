569faca2e4840e2dad89ede3ed547d2a0198823f
nastyatal04 <anastasia691518@gmail.com>
2024-01-22 17:51:46 +0300

    Added gradient to button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export const ButtonNoBackground = ({ text, width = "w-max" }) => {
   return (
-    <div
-      className={`font-bold bg-black-rock text-center py-3 px-10 border-2 border-fashion-fuchsia text-white border-solid cursor-pointer rounded-lg ${width}`}
-    >
-      {text}
+    <div className="relative">
+      <div className="bg-gradient-to-r from-[rgba(255,255,255,0)] w-full h-full to-[rgba(5,4,34,0.8)] absolute top-0 left-0 rounded-lg"></div>
+      <div
+        className={`font-bold bg-[rgb(24,19,54)] text-center py-3 px-10 border-2 border-fashion-fuchsia text-white border-solid cursor-pointer rounded-lg ${width}`}
+      >
+        <span className="relative">{text}</span>
+      </div>
     </div>
   );
 };
@@ -23,7 +26,7 @@ export const ButtonWithBackground = ({ text, width = "w-max" }) => {
 export const ButtonWithBackgroundBlue = ({ text, width = "w-max" }) => {
   return (
     <div
-      className={`font-bold text-white text-center bg-gradient-to-r from-[rgba(30,225,225,0.9)] to-[rgba(24, 254, 255, 0.00)] py-3 px-10 rounded-full cursor-pointer ${width}`}
+      className={`font-bold text-white text-center bg-gradient-to-r from-[rgb(13,106,124)] to-[rgba(24, 254, 255, 0.00)] py-3 px-10 rounded-full cursor-pointer ${width}`}
     >
       {text}
     </div>
--- src/pages/ModernCryptoSelling.jsx
+++ src/pages/ModernCryptoSelling.jsx
@@ -74,6 +74,36 @@ export const ModernCryptoSelling = () => {
           ))}
         </div>
       </div>
+      <div className="flex flex-col items-center gap-6">
+        <div className="flex flex-row gap-4">
+          <AccentText text="Browse by" />
+          <Heading text="Category" />
+        </div>
+        <div className="font-normal text-[20px] text-light-gray w-2/5 text-center">
+          Set a secondary sales fee and add social links, a description, profile
+          and banner images, and a description.
+        </div>
+        <div className="flex flex-wrap gap-8">
+          <img
+            src="https://avatars.mds.yandex.net/i?id=00dd7ff40347a3408954b0ae7fa12035c9e5506d-9181886-images-thumbs&n=13"
+            alt=""
+            className="aspect-square rounded-2xl h-[400px]"
+          />
+          <img
+            src="https://www.forexscamreview.com/wp-content/uploads/2023/09/3767-what-is-capital-formation-in-forex-trading.jpg"
+            alt=""
+            className="rounded-2xl h-[400px]"
+          />
+          <img
+            src="https://m.media-amazon.com/images/S/aplus-media-library-service-media/d2db4de9-c6f4-4d82-b850-20eb23f2cad2.__CR0,291,1800,768_PT0_SX1464_V1___.jpg"
+            alt=""
+          />
+          <img
+            src="https://i.pinimg.com/originals/12/e7/26/12e726e230f10eafae492579bd3d2405.jpg"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
